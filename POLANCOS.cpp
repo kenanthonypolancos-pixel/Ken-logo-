e1b4f95331f67fa4aa5662bc7c866ec217433971
kenanthonypolancos-pixel <kenanthony.polancos@hcdc.edu.ph>
2025-09-24 10:56:29 +0800

    Add files via upload

---
+++ POLANCOS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "KKKK    KKKKK\n";
+    std::cout << "KKKK   KKKKK\n";
+    std::cout << "KKKK  KKKKK\n";
+    std::cout << "KKKK KKKKK\n";
+    std::cout << "KKKKKK\n";
+    std::cout << "KKKK KKKKK\n";
+    std::cout << "KKKK  KKKKK\n";
+    std::cout << "KKKK   KKKKK\n";
+    std::cout << "KKKK    KKKKK\n";
+    return 0;
+}
